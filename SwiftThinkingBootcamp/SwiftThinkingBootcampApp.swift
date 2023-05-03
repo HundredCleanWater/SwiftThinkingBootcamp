3742f4f7e68f230585a5debf6512ea80381a9e54
HundredCleanWater <maxmp9904@gmail.com>
2023-05-03 23:39:26 +0900

    Initial Commit

---
+++ SwiftThinkingBootcamp/SwiftThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftThinkingBootcampApp.swift
+//  SwiftThinkingBootcamp
+//
+//  Created by 백정수 on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
